3383d1ccd867dae5926ad905d0a25de1dd4ff29b
Manuel Olivera <manuod95@gmail.com>
2023-08-24 04:43:38 +0200

    se modificó todo para actualizar el sitio

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+/* Código para que se despliegue el menú en el formato responsive */
 document.addEventListener('DOMContentLoaded', function() {
     const mobileMenuButton = document.getElementById('mobile-menu-button');
     const mobileMenu = document.querySelector('.mobile-menu');
@@ -5,4 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
     mobileMenuButton.addEventListener('click', function() {
         mobileMenu.classList.toggle('show');
     });
+});
+
+
+/* Código para que aparezca el footer al llegar al final de la página */
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const body = document.body;
+
+    mobileMenuButton.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show');
+    });
+
+    // Agrega este código para mostrar el footer al llegar al final
+    const footer = document.querySelector('footer');
+    const main = document.querySelector('main');
+    const windowHeight = window.innerHeight;
+
+    function toggleFooter() {
+        const scrollTop = window.scrollY || window.pageYOffset;
+        const mainHeight = main.offsetHeight;
+        
+        if (scrollTop + windowHeight >= mainHeight) {
+            body.classList.add('show-footer');
+        } else {
+            body.classList.remove('show-footer');
+        }
+    }
+
+    toggleFooter();
+    window.addEventListener('scroll', toggleFooter);
 });
