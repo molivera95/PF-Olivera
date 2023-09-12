eba50f32d7fc00ef57002f303a41ec1586d2d1f1
Manuel Olivera <manuod95@gmail.com>
2023-09-12 02:45:08 +0200

    penúltimos cambios del sitio

--- js/javascript.js
+++ js/javascript.js
@@ -7,36 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
         mobileMenu.classList.toggle('show');
     });
 
-    // Agrega este código para mostrar el footer al llegar al final
-    const footer = document.querySelector('footer');
-    const main = document.querySelector('main');
-    const windowHeight = window.innerHeight;
-    
-    function toggleFooter() {
-        const scrollTop = window.scrollY || window.pageYOffset;
-        const mainHeight = main.offsetHeight;
-        
-        if (scrollTop + windowHeight >= mainHeight) {
-            document.body.classList.add('show-footer');
-        } else {
-            document.body.classList.remove('show-footer');
-        }
-    }
-    
-    toggleFooter();
-    window.addEventListener('scroll', toggleFooter);
-});
-
-// Código para mostrar el footer al llegar al final de la página
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const body = document.body;
-
-    mobileMenuButton.addEventListener('click', function() {
-        mobileMenu.classList.toggle('show');
-    });
-
     // Función para mostrar el footer al llegar al final
     const footer = document.querySelector('footer');
     const main = document.querySelector('main');
@@ -47,9 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const mainHeight = main.offsetHeight;
         
         if (scrollTop + windowHeight >= mainHeight) {
-            body.classList.add('show-footer');
+            document.body.classList.add('show-footer');
         } else {
-            body.classList.remove('show-footer');
+            document.body.classList.remove('show-footer');
         }
     }
 
