8f3ba5b696755f4e6ca4a704afb4b012774151ba
Manuel Olivera <manuod95@gmail.com>
2023-09-24 03:04:47 +0200

    Corregido el menú mobile

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,10 @@
 // Código para manejar el menú desplegable en el formato mobile
 document.addEventListener('DOMContentLoaded', function() {
     const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const header = document.querySelector('header');
     const mobileMenu = document.querySelector('.mobile-menu');
 
     mobileMenuButton.addEventListener('click', function() {
-        mobileMenu.classList.toggle('show');
+        header.classList.toggle('show');
     });
 });
